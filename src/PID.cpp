a2c478322b8a81a359beba0a269002196bb7b19e
PraveenKumar-Rajendran <aravindhanpraveen19@gmail.com>
2021-03-05 01:48:27 +0900

    feat: complete PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,6 +12,15 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+  
+  // Initialize coeffs with the passing arguments
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+  // Initializing PID Errors to zero
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0; 
 
 }
 
@@ -20,11 +29,32 @@ void PID::UpdateError(double cte) {
    * TODO: Update PID errors based on cte.
    */
 
+  // Differential error.
+  d_error = cte - p_error;  // difference in the Cross track error between current and previous time
+  // Proportional error
+  p_error = cte;            // Cross track error at a given time
+  // Integral error
+  i_error += cte;           // Cross track error accumlated over the entire time
+
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+
+  return p_error * Kp + i_error * Ki + d_error * Kd;  // TODO: Add your total error calc here!
+}
+
+double PID::ControlInput() {
+  /**
+   * TODO: Calculate control input for steering using PID
+   */
+  
+  double control_input = -Kp * p_error - Kd * d_error - Ki * i_error;
+// steering value must be between [-1, 1]
+  control_input = control_input < -1 ? -1 : control_input;  // For the lower limit
+  control_input = control_input > 1 ? 1 : control_input; // For the upper limit
+ 
+  return control_input;  // TODO: Add your total error calc here!
 }
