dc5245e31bf691f7d8fbeee7f2e1a284ee7ebb2f
PraveenKumar-Rajendran <aravindhanpraveen19@gmail.com>
2021-03-05 01:52:46 +0900

    feat: edit PID.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,9 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
+  
+  pid.Init(0.15, 0.0001, 1.5);
+
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
@@ -64,6 +67,9 @@ int main() {
            *   Maybe use another PID controller to control the speed!
            */
           
+          pid.UpdateError(cte);
+          steer_value = pid.ControlInput();
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
                     << std::endl;
